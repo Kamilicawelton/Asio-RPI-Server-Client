11d18bb9db4dbf6cfb514969be7817feb81464ec
Leonard Fabris <83458396+LFabris6@users.noreply.github.com>
2021-10-20 17:55:31 +0200

    Update jpgtochar.cpp

--- jpgtochar.cpp
+++ jpgtochar.cpp
@@ -3,7 +3,49 @@
 #include <iostream>
 #include <fstream>
 
+size_t filesize;
+socket.send(boost::asio::buffer(JpgToChar(filesize), filesize));
+
+ unsigned char buff[11585];
+            size_t bytes_transferred = socket.receive(boost::asio::buffer(buff));
+            size_t size = sizeof(buff) / sizeof(buff[0]);
+            BufferToJpg(buff, size);
+
+
 using namespace std;
+
+void BufferToJpg(unsigned char* output, size_t &size) {
+
+    std::ofstream fout("D:/slika.jpg", std::ios::binary);
+    if (!fout.good())
+        return;
+    fout.write((char*)output, size);
+
+
+}
+
+unsigned char* JpgToChar(size_t &filesize) {
+
+    
+    std::ifstream DataFile("D:/Programiranje/fileovi/slikica.jpg", std::ios::binary);
+    if (!DataFile.good())
+        return 0;
+
+    DataFile.seekg(0, std::ios::end);
+    filesize = (int)DataFile.tellg();
+    DataFile.seekg(0);
+
+    
+    
+     unsigned char *output = new unsigned char[filesize];
+     DataFile.read((char*)output, filesize);
+    
+
+    return output;
+
+
+}
+
 
 int main(){
 
